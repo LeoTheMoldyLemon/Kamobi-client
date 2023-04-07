cfcf4954431f37678983c056b6ef18cc3f06cd97
leonardo simunovic <leonardo.simunovic@gmail.com>
2023-04-07 20:03:12 +0200

    Popravio naming i dodao neke stvari za frendove

---
+++ Kamobi/Kamobi/Views/AddFriendsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kamobi.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddFriendsPage : ContentPage
+    {
+        public AddFriendsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void AddFriendClicked(object sender, EventArgs e)
+        {
+            string search = "NekiEntry.Text";
+            if (search.Length == 0)
+            {
+                Navigation.ShowPopup(new InfoPopup("Please enter a valid username or phone number."));
+                return;
+            }
+            LoadingPopup loading = new LoadingPopup();
+            Navigation.ShowPopup(loading);
+            var data = JsonNode.Parse("{}");
+            data["search"] = search;
+            JsonNode returnData = await App.socket.sendRequest("loginUser", data, 20000);
+            loading.Dismiss(null);
+            if (!(bool)returnData["success"])
+            {
+                Navigation.ShowPopup(new InfoPopup((string)returnData["error"]["description"]));
+                return;
+            }
+
+        }
+    }
+}
