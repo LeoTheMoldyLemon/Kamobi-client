62379ad0d8e2ed29704e2d27f5e21757852e3d37
KombatKingo <5arknezevic@gmail.com>
2023-04-09 22:40:37 +0200

    jeste ikad sanjali

--- Kamobi/Kamobi/Views/ProfilePage.xaml.cs
+++ Kamobi/Kamobi/Views/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kamobi.Services;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -28,5 +29,28 @@ namespace Kamobi.Views
             loading.Dismiss(null);
             await Shell.Current.GoToAsync("//LoginPage");
         }
+        private async void ChangeUsernameButtonClicked(object sender, EventArgs e)
+        {
+            LoadingPopup loading = new LoadingPopup();
+            Navigation.ShowPopup(loading);
+            var data = JsonNode.Parse("{}");
+            var random = new Random((int)DateTime.Now.Ticks);
+            data["username"] = "noviUN" + ((random.Next(0, 10000)).ToString()).PadLeft(4, ' ');
+            JsonNode returnData = await App.socket.sendRequest("updateUser", data, 20000);
+            loading.Dismiss(null);
+            if (returnData == null)
+            {
+                Navigation.ShowPopup(new InfoPopup("Server response timed out. Please try again later."));
+                return;
+            }
+            if (!(bool)returnData["success"])
+            {
+                Navigation.ShowPopup(new InfoPopup((string)returnData["error"]["description"]));
+                return;
+            }
+            UserInfo.username = (string)data["username"];
+            UserInfo.displayname = UserInfo.username.Substring(0, UserInfo.username.Length - 5);
+        }
+
     }
 }
