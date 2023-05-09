5fe65159cb6dc67987083e6b0f29d6789c7bc8f3
LeoTheMoldyLemon <leonardo.simunovic@gmail.com>
2023-05-09 09:57:08 +0200

    working drag-n-drop, todo frontend

--- Kamobi/Kamobi/Views/TierlistPage.xaml.cs
+++ Kamobi/Kamobi/Views/TierlistPage.xaml.cs
@@ -12,18 +12,25 @@ namespace Kamobi.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TierlistPage : ContentPage
     {
+       
         public TierlistPage()
         {
             InitializeComponent();
         }
         private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
         {
-
+            e.Data.Properties.Add("Element", (sender as Element).Parent as Frame);
         }
 
         private async void FinishButton_Clicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//ProfilePage");
+        }
+
+        private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+        {
+           
+            ((StackLayout)(sender as Element).Parent).Children.Add((Frame)e.Data.Properties["Element"]);
         }
     }
 }
